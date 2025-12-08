31bd1222ad9438f12ccac336a4aa39e36f609c57
TufelMalik <mtufel21@gmail.com>
2025-12-08 20:10:12 +0530

    Creating new project.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,18 +56,12 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(libs.androidx.navigation.compose)
-            implementation(libs.kotlinx.datetime)
-            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutinesSwing)
-            implementation(libs.kotlinx.datetime)
-            implementation(libs.kotlinx.serialization.json)
         }
     }
 }
